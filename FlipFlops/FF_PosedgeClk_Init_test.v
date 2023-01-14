a8b5a55bccb45608dc38a91fe5f4cd319b210e0d
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-14 22:27:39 +0330

    Rename FF_PosedgeClk_InitState_test.v to FF_PosedgeClk_Init_test.v

---
+++ FlipFlops/FF_PosedgeClk_Init_test.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+
+module FF_pos_edge_clk_init_test;
+
+	// Inputs
+	reg D;
+	reg clk;
+
+	// Outputs
+	wire Q;
+
+	// Instantiate the Unit Under Test (UUT)
+	FF_pos_edge_clk_initState uut (
+		.D(D), 
+		.clk(clk), 
+		.Q(Q)
+	);
+
+	initial begin
+		// Initialize Inputs
+		D = 0;
+		forever #(50) clk = ~clk;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		
+		D = 1;
+		
+		#100;
+		
+		D = 0;
+		
+		#100;
+		
+		D = 1;
+
+	end
+      
+endmodule
+
