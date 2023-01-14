56c9e91a8a73129fee09abff0cd03edf86cb7608
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-14 20:49:10 +0330

    Update D_FF_test.v

--- FlipFlops/D_FF_test.v
+++ FlipFlops/D_FF_test.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   21:21:05 12/18/2022
-// Design Name:   D_FF
-// Module Name:   C:/Users/fereshteh/Flip_Flops/D_FF_test.v
-// Project Name:  Flip_Flops
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: D_FF
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module D_FF_test;
 
 	// Inputs
