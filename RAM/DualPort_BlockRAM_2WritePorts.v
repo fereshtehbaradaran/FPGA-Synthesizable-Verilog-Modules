6839c4132b7a82c92007f6715e5ce43de5c3b6e0
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-14 22:57:34 +0330

    Update and rename dualPort_blockRAM_2writePorts.v to DualPort_BlockRAM_2WritePorts.v

--- RAM/DualPort_BlockRAM_2WritePorts.v
+++ RAM/DualPort_BlockRAM_2WritePorts.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:04:28 01/12/2023 
-// Design Name: 
-// Module Name:    dualPort_blockRAM_2writePorts 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module dualPort_blockRAM_2writePorts(
     input CLK1,
     input CLK2,
