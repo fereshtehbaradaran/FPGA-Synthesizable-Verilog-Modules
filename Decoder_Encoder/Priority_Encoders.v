1aab100220a1088fb51a50efdc3374d657fae140
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-14 19:24:14 +0330

    Update and rename priority_encoders.v to Priority_Encoders.v

--- Decoder_Encoder/Priority_Encoders.v
+++ Decoder_Encoder/Priority_Encoders.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    09:09:45 01/12/2023 
-// Design Name: 
-// Module Name:    priority_encoders 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 (*priority_extract="force"*)
 
 module priority_encoders(
