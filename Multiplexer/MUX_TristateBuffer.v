626524b077e197aec271a019b79443dc4596a078
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-14 22:40:00 +0330

    Update MUX_TristateBuffer.v

--- Multiplexer/MUX_TristateBuffer.v
+++ Multiplexer/MUX_TristateBuffer.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    08:53:25 01/12/2023 
-// Design Name: 
-// Module Name:    MUX_TristateBuffer 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module MUX_TristateBuffer(
     input [1:0] S,
     input A,
