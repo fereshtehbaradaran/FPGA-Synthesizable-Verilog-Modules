54636632e646cd83fa8ebde211fd5fc396e4d237
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-14 20:48:45 +0330

    Update D_FF.v

--- FlipFlops/D_FF.v
+++ FlipFlops/D_FF.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    21:11:19 12/18/2022 
-// Design Name: 
-// Module Name:    D_FF 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module D_FF(
     input D,
     input clk,
