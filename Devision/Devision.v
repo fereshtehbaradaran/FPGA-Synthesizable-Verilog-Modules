e718ad71df88409bba9bd92b0f2c45fdc02ca142
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-14 20:45:41 +0330

    Update Devision.v

--- Devision/Devision.v
+++ Devision/Devision.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    12:03:31 01/12/2023 
-// Design Name: 
-// Module Name:    Devision 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Devision(
     input [7:0] DI,
     output [7:0] DO
