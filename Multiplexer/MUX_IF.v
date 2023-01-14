e9e940bf8ec3c9453edda23038c14c7ea7d2fd24
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-14 22:39:38 +0330

    Update MUX_IF.v

--- Multiplexer/MUX_IF.v
+++ Multiplexer/MUX_IF.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    08:44:30 01/12/2023 
-// Design Name: 
-// Module Name:    MUX_IF 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module MUX_IF(
     input A,
     input B,
